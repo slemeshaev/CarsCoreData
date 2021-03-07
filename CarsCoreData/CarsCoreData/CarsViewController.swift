17a6d37077995f9140b9b1f40fb9fbceb1736b82
slemeshaev <lemeshaevs@gmail.com>
2021-03-07 07:41:02 +0300

    save in coredata

--- CarsCoreData/CarsCoreData/CarsViewController.swift
+++ CarsCoreData/CarsCoreData/CarsViewController.swift
@@ -44,6 +44,60 @@ class CarsViewController: UIViewController {
     }
     
     // MARK: - Helpers
+    
+    private func getDataFromFile() {
+        
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
+        var records = 0
+        
+        do {
+            records = try context.count(for: fetchRequest)
+            print("Is Data there already?")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        guard records == 0 else { return }
+        
+        guard let pathToFile = Bundle.main.path(forResource: "DataCars", ofType: "plist"),
+              let dataArray = NSArray(contentsOfFile: pathToFile) else {
+            print("Error retrieving data from file")
+            return
+        }
+        
+        for dictionary in dataArray {
+            guard let entity = NSEntityDescription.entity(forEntityName: "Car", in: context) else { return }
+            let car = NSManagedObject(entity: entity, insertInto: context) as! Car
+            
+            let carDictionary = dictionary as! [String: AnyObject]
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            
+            guard let imageName = carDictionary["imageName"] as? String else { return }
+            let image = UIImage(named: imageName)
+            let imageData = image?.pngData()
+            car.imageData = imageData
+            
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.rating = carDictionary["rating"] as! Double
+            car.timesDriven = carDictionary["timesDriven"] as! Int16
+            car.myChoice = carDictionary["moChoice"] as! Bool
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String: Float] {
+                car.tintColor = getColor(colorDictionary: colorDictionary)
+            }
+            
+        }
+    }
+    
+    private func getColor(colorDictionary: [String: Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+              let green = colorDictionary["green"],
+              let blue = colorDictionary["blue"] else { return UIColor() }
+        return UIColor(red: CGFloat(red / 255), green: CGFloat(green / 255), blue: CGFloat(blue / 255), alpha: 1.0)
+    }
 
 
 }
